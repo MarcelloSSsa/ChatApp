b6edbc35e8ceba5e5950ad2e887680f047fd8baf
Marcello Sousa <marcello_cte@hotmail.com>
2018-06-16 22:44:36 -0300

    create user login redirect 2 (install Drawable plugin)

--- app/src/main/java/falaai/app/com/falaai/Activity/MainActivity.java
+++ app/src/main/java/falaai/app/com/falaai/Activity/MainActivity.java
@@ -1,7 +1,10 @@
 package falaai.app.com.falaai.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -9,9 +12,25 @@ import falaai.app.com.falaai.R;
 
 public class MainActivity extends Activity {
 
+    private Button sair;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        sair = findViewById(R.id.botaoSair);
+
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
